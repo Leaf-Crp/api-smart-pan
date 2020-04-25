06c17b5b93f8065a4d25def5e7efd75398fa6b67
Maxime Certain <MaximeCertain>
2020-04-25 18:45:24 +0200

    enregistrement image ds api, cleaning childs recette persos

--- app/controllers/recipe.controller.js
+++ app/controllers/recipe.controller.js
@@ -1,6 +1,8 @@
 import db from "../../models";
 import GlobalConstants from "../constants/global.constants";
 import RecipeAssociations from "../constants/recipe.constants";
+const multer = require('multer');
+const upload = multer({dest:'public/uploads/recipes'}).single("recipe_image");
 
 
 class RecipeController {
@@ -9,7 +11,7 @@ class RecipeController {
         let body = {};
         try {
             let recipes = await db.recipe.findAll(RecipeAssociations.RECIPE_ASSOCIATIONS);
-          //on accède aux propriétés mtm directement dans l'objet
+            //on accède aux propriétés mtm directement dans l'objet
             recipes.map(r => {
                 r.steps.map(s => {
                     s.ingredients.map(i => {
@@ -32,37 +34,48 @@ class RecipeController {
         let status = 200;
         let body = [];
         try {
-            let recipeToCreate = {
-                label: request.body.label,
-                image: request.body.image,
-                is_private: request.body.is_private,
-                id_recipe_type: request.body.id_recipe_type,
-                id_user: request.body.id_user,
-                steps: request.body.steps
-            };
-            let recipe = await db.recipe.create(recipeToCreate, {include: "steps"});
-            (recipe.steps.map(async s => {
-                let stepIngredient = {
-                    id_step: s.id,
-                    id_ingredient: 1,
-                    quantity: 1
-                };
 
-                await db.step_ingredient.create(stepIngredient);
+        let recipeToCreate = {
+            label: request.body.label,
+            image: request.body.image,
+            is_private: request.body.is_private,
+            id_recipe_type: request.body.id_recipe_type,
+            id_user: request.body.id_user
+        };
+    /*    let fileName = "";
+          await upload(request,response, (err)=>{
+              let fileName = (request.file.destination + '/'+ request.file.filename);
+              console.log(fileName);
+          });*/
 
-                let prerequisiteTypeStep = {
-                    id_step: s.id,
-                    id_prerequisite_type: 1,
-                    detail: "1"
-                };
-                await db.prerequisite_type_step.create(prerequisiteTypeStep);
-            }));
-            body = {'recipeCreated': recipe, 'recipe': 'created'};
-        } catch (error) {
-            console.log(error);
-            status = 500;
-            body = {'recipe': error.recipe};
-        }
+               let recipe = await db.recipe.create(recipeToCreate);
+               request.body.steps.map(async step => {
+                   step.id_recipe = recipe.id;
+                   let stepCreated = await db.step.create(step);
+                   step.ingredients.map(async ingredient => {
+                       let step_ingredient = {
+                           id_step: stepCreated.id,
+                           id_ingredient: ingredient.id,
+                           quantity: ingredient.quantity
+                       };
+                       await db.step_ingredient.create(step_ingredient);
+                   });
+                   step.prerequisiteTypes.map(async prerequisite => {
+                       let prerequisite_type_step = {
+                           id_step: stepCreated.id,
+                           id_prerequisite_type: prerequisite.id,
+                           detail: prerequisite.detail
+                       };
+                       await db.prerequisite_type_step.create(prerequisite_type_step);
+                   })
+               });
+
+               body = {'recipeCreated': 12, 'recipe': 'created'};
+           } catch (error) {
+               console.log(error);
+               status = 500;
+               body = {'recipe': error.recipe};
+           }
         return response.status(status).json(body);
     }
 
--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -1,5 +1,3 @@
-INSERT INTO `user` (`id`, `email`, `password`, `firstname`, `lastname`, `is_connected_pan`, `alarm_ended_recipe`, `alarm_ended_step`) VALUES ('1', 'vincenzo.nibali@astana.it', 'nibali', 'Vincenzo', 'Nibali', '1', '', '');
-INSERT INTO `ingredient` (`id`, `label`, `image`) VALUES ('1', 'carotte', '');
 
 INSERT INTO `prerequisite_type` (`id`, `label`, `code`) VALUES ('1', 'Température minimale à atteindre', 'min_temperature');
 INSERT INTO `prerequisite_type` (`id`, `label`, `code`) VALUES ('2', 'Température maximale à ne pas dépasser', 'max_temperature');
@@ -12,19 +10,26 @@ INSERT INTO smart_pan.ingredient (id, image, label) VALUES (3, 'lettuce', 'Salad
 INSERT INTO smart_pan.ingredient (id, image, label) VALUES (4, 'meat', 'Boeuf');
 INSERT INTO smart_pan.ingredient (id, image, label) VALUES (5, 'chicken', 'Poulet');
 INSERT INTO smart_pan.ingredient (id, image, label) VALUES (6, 'spices', 'Epices');
-INSERT INTO smart_pan.prerequisite_type (id, label, code) VALUES (1, 'Température minimale à atteindre', 'min_temperature');
-INSERT INTO smart_pan.prerequisite_type (id, label, code) VALUES (2, 'Température maximale à ne pas dépasser', 'max_temperature');
-INSERT INTO smart_pan.prerequisite_type (id, label, code) VALUES (3, 'Temps d''attente minimum entre deux étapes', 'waiting_time_before_step');
+INSERT INTO `ingredient` (`id`, `label`, `image`) VALUES ('7', 'carotte', 'carotte');
+
+INSERT INTO smart_pan.recipe_type (id, label) VALUES (1, 'Fast Food');
+INSERT INTO smart_pan.recipe_type (id, label) VALUES (2, 'Dessert');
+INSERT INTO smart_pan.recipe_type (id, label) VALUES (3, 'Entrée');
+
+INSERT INTO smart_pan.user (id, email, password, firstname, lastname, is_connected_pan, alarm_ended_recipe, alarm_ended_step) VALUES (1, 'vincenzo.nibali@astana.it', 'nibali', 'Vincenzo', 'Nibali', 1, '', '');
+INSERT INTO smart_pan.user (id, email, password, firstname, lastname, is_connected_pan, alarm_ended_recipe, alarm_ended_step) VALUES (2, 'sdfsfdfs', '$2b$10$6kWUiZlSScibGbHtj34IVuxxOVQsKJOGNb1h5eIfDetbtCknCyseO', 'sdfsfdfs', 'sdfsfdfs', 1, 'sdfsfdfs', 'sdfsfdfs');
+
 INSERT INTO smart_pan.recipe (id, label, image, is_private, id_recipe_type, id_user) VALUES (1, 'Kebab', 'kebab', 1, 1, 1);
 INSERT INTO smart_pan.recipe (id, label, image, is_private, id_recipe_type, id_user) VALUES (2, 'Fried Chiken', 'food2', 1, 1, 1);
 INSERT INTO smart_pan.recipe (id, label, image, is_private, id_recipe_type, id_user) VALUES (3, 'Salad', 'food3', 1, 1, 1);
-INSERT INTO smart_pan.recipe_type (id, label) VALUES (1, 'Fast Food');
+
 INSERT INTO smart_pan.step (id, label, duration, id_recipe) VALUES (1, 'Couper les legumes', 1, 1);
 INSERT INTO smart_pan.step (id, label, duration, id_recipe) VALUES (2, 'Couper le poulet', 1, 2);
 INSERT INTO smart_pan.step (id, label, duration, id_recipe) VALUES (3, 'Assaisonner la viande', 1, 1);
 INSERT INTO smart_pan.step (id, label, duration, id_recipe) VALUES (5, 'chauffer poele ', 5, 2);
 INSERT INTO smart_pan.step (id, label, duration, id_recipe) VALUES (7, 'Couper la viande', 1, 1);
 INSERT INTO smart_pan.step (id, label, duration, id_recipe) VALUES (8, 'test', 1, 3);
+
 INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES (1, 1, 1);
 INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES (1, 2, 1);
 INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES (1, 3, 1);
@@ -33,5 +38,3 @@ INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES 
 INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES (3, 6, 1);
 INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES (7, 4, 1);
 INSERT INTO smart_pan.step_ingredient (id_step, id_ingredient, quantity) VALUES (7, 5, 1);
-INSERT INTO smart_pan.user (id, email, password, firstname, lastname, is_connected_pan, alarm_ended_recipe, alarm_ended_step) VALUES (1, 'vincenzo.nibali@astana.it', 'nibali', 'Vincenzo', 'Nibali', 1, '', '');
-INSERT INTO smart_pan.user (id, email, password, firstname, lastname, is_connected_pan, alarm_ended_recipe, alarm_ended_step) VALUES (2, 'sdfsfdfs', '$2b$10$6kWUiZlSScibGbHtj34IVuxxOVQsKJOGNb1h5eIfDetbtCknCyseO', 'sdfsfdfs', 'sdfsfdfs', 1, 'sdfsfdfs', 'sdfsfdfs');
